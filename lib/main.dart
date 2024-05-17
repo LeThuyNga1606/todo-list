cc935ae923f2b7b5ea15241ea41a16b83f45d655
LeThuyNga1606 <lethuynga1662002@gmail.com>
2024-05-17 19:56:38 +0700

    update layout

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'TODO App',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.purple,
       ),
       home: const TodoHomePage(),
     );
